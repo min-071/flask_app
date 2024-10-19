bd7844fe1701e00593a4d99679502c5edf67f0f6
min <92686513+min-071@users.noreply.github.com>
2024-10-18 22:02:21 -0400

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from pymongo import MongoClient
 from flask import render_template
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")
+MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
 
 
 app = Flask(__name__)
